96c6bbfd417223b73414329ff2a6f140fa5e7484
Kelsey Freeborg <kelsey.freeborg@gmail.com>
2021-03-05 19:46:47 -0700

    user and book added to typedefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,5 +1,22 @@
 const { gql } = require("apollo-server-express");
 
-const typeDefs = gql``;
+const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
+
+  type Book {
+    authors: [String]
+    description: String
+    bookId: String
+    image: String
+    link: String
+    title: String
+  }
+`;
 
 module.exports = typeDefs;
