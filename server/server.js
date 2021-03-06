86422cf2a7c3ecb1d4902ec2558d9e86ffc82d5d
Kelsey Freeborg <kelsey.freeborg@gmail.com>
2021-03-05 19:15:47 -0700

    schemas folder added

--- server/server.js
+++ server/server.js
@@ -1,24 +1,21 @@
 const express = require("express");
 const path = require("path");
-// import ApolloServer
-// const { ApolloServer } = require("apollo-server-express");
-
-// import our typeDefs and resolvers
-// const { typeDefs, resolvers } = require("./schemas");
+const { ApolloServer } = require("apollo-server-express");
+const { authMiddleware } = require("./utils/auth");
+const { typeDefs, resolvers } = require("./schemas");
 const db = require("./config/connection");
 const routes = require("./routes");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// create a new Apollo server and pass in our schema data
-// const server = new ApolloServer({
-//   typeDefs,
-//   resolvers,
-// });
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+});
 
-// integrate our Apollo server with the Express application as middleware
-// server.applyMiddleware({ app });
+server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
