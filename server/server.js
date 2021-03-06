dbac3ea47303bc8de53cc78289e1ea49645ddae6
Kelsey Freeborg <kelsey.freeborg@gmail.com>
2021-03-05 19:05:43 -0700

    apollo set up in server

--- server/server.js
+++ server/server.js
@@ -1,21 +1,35 @@
-const express = require('express');
-const path = require('path');
-const db = require('./config/connection');
-const routes = require('./routes');
+const express = require("express");
+const path = require("path");
+// import ApolloServer
+// const { ApolloServer } = require("apollo-server-express");
+
+// import our typeDefs and resolvers
+// const { typeDefs, resolvers } = require("./schemas");
+const db = require("./config/connection");
+const routes = require("./routes");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+// create a new Apollo server and pass in our schema data
+// const server = new ApolloServer({
+//   typeDefs,
+//   resolvers,
+// });
+
+// integrate our Apollo server with the Express application as middleware
+// server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
 app.use(routes);
 
-db.once('open', () => {
+db.once("open", () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 });
