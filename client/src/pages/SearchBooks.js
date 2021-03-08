3c0f92cf4cd75ae2aaed2b8cf1ef8da76e22db7b
Kelsey Freeborg <kelsey.freeborg@gmail.com>
2021-03-07 17:06:40 -0700

    search books edited

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -78,11 +78,14 @@ const SearchBooks = () => {
     }
 
     try {
-      const response = await saveBook(bookToSave, token);
+      const { data } = await saveBook({
+        variables: { input: bookToSave },
+      });
 
-      if (!response.ok) {
+      if (error) {
         throw new Error("something went wrong!");
       }
+      console.log(data);
 
       // if book successfully saves to user's account, save book id to state
       setSavedBookIds([...savedBookIds, bookToSave.bookId]);
