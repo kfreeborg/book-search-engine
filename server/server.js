f6d50de53821f1a282cc0a317f2f2aa513f42df4
Kelsey Freeborg <kelsey.freeborg@gmail.com>
2021-03-08 19:37:54 -0700

    remove routes

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const { ApolloServer } = require("apollo-server-express");
 const { authMiddleware } = require("./utils/auth");
 const { typeDefs, resolvers } = require("./schemas");
 const db = require("./config/connection");
-const routes = require("./routes");
+// const routes = require("./routes");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -24,7 +24,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
-app.use(routes);
+// app.use(routes);
 
 db.once("open", () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
